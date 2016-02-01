ccdc94f71b3c55be9cd45d5ae95f79dac2715ee6
Tyler-Wang <983243283@qq.com>
2016-02-01 11:56:11 +0800

    add log4j and modify web.xml

---
+++ smsi-parent/smsi-web/src/main/java/com/smsi/web/listener/SessionListener.java
@@ -0,0 +1,18 @@
+package com.smsi.web.listener;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+public class SessionListener implements HttpSessionListener {
+
+	public void sessionCreated(HttpSessionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void sessionDestroyed(HttpSessionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
