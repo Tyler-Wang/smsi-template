a5f205c919d1ffb6f2c6f50d3dbac92671143bad
Tyler-Wang <983243283@qq.com>
2016-03-10 23:49:56 +0800

    learn springmvc

--- smsi-parent/smsi-web/src/main/java/com/smsi/web/controller/login/LoginController.java
+++ smsi-parent/smsi-web/src/main/java/com/smsi/web/controller/login/LoginController.java
@@ -1,11 +1,15 @@
 package com.smsi.web.controller.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-	
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+	@RequestMapping
 	public void login(){
-		
+		logger.info("login");
 	}
 }
