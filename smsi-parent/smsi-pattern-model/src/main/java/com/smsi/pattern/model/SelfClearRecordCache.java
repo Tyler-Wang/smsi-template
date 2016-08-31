ac9ec05e3abca4b016c4ec5083cb17c6bb8f9002
Tyler-Wang <983243283@qq.com>
2016-08-31 17:21:38 +0800

    SelfClearRecordCache replace RecordCache

--- smsi-parent/smsi-pattern-model/src/main/java/com/smsi/pattern/model/SelfClearRecordCache.java
+++ smsi-parent/smsi-pattern-model/src/main/java/com/smsi/pattern/model/SelfClearRecordCache.java
@@ -14,7 +14,7 @@ import org.slf4j.LoggerFactory;
  * @author wangj
  * @date 2016年6月24日
  */
-public class RecordCache {
+public class SelfClearRecordCache<K, V> {
 	private static final int DEFAULT_CACHE_SIZE = 10000;//默认缓存大小
 	private static final int DEFAULT_CLEAR_PERIOD = 2;//默认清理间隔，2分钟
 	private static final int DEFAULT_RECORD_EXPIRY_TIME = 5; //默认缓存记录失效时间，5分钟
@@ -23,38 +23,38 @@ public class RecordCache {
 	private int clearPeriod; //清理间隔
 	private int recordExpiryTime; //缓存记录失效时间
 	
-	private ConcurrentHashMap<String, Date> cache;//缓存
+	private ConcurrentHashMap<K, CacheData> cache;//缓存
 	private ScheduledExecutorService schExecutor;
-	private static final Logger logger = LoggerFactory.getLogger(RecordCache.class);
+	private static final Logger logger = LoggerFactory.getLogger(SelfClearRecordCache.class);
 	
-	public RecordCache(String cacheName, int cacheSize, int clearPeriod, int recordExpiryTime){
+	public SelfClearRecordCache(String cacheName, int cacheSize, int clearPeriod, int recordExpiryTime){
 		if(!(cacheSize > 0 && clearPeriod > 0 && recordExpiryTime > 0))
 			throw new IllegalArgumentException("参数必须是正整数");
 		this.cacheName = cacheName;
 		this.clearPeriod = clearPeriod;
 		this.recordExpiryTime = recordExpiryTime;
-		this.cache = new ConcurrentHashMap<String, Date>(cacheSize);
+		this.cache = new ConcurrentHashMap<K, CacheData>(cacheSize);
 		schExecutor = Executors.newSingleThreadScheduledExecutor(new CustomThreadFactory(cacheName));
 		schExecutor.scheduleAtFixedRate(new ClearTask(), 1, this.clearPeriod, TimeUnit.MINUTES);
 	}
 	
-	public RecordCache(String cacheName){
+	public SelfClearRecordCache(String cacheName){
 		this(cacheName,DEFAULT_CACHE_SIZE,DEFAULT_CLEAR_PERIOD,DEFAULT_RECORD_EXPIRY_TIME);
 	}
 	
-	public  RecordCache(String cacheName, int clearPeriod,  int recordExpiryTime){
+	public  SelfClearRecordCache(String cacheName, int clearPeriod,  int recordExpiryTime){
 		this(cacheName, DEFAULT_CACHE_SIZE, clearPeriod, recordExpiryTime);
 	}
 	
-	public void put(String key){
-		this.cache.put(key, new Date());
+	public void put(K key, V value){
+		this.cache.put(key, new CacheData(value));
 	}
 	
-	public void put(String key, Date date){
-		this.cache.put(key, date);
+	public void put(K key){
+		this.cache.put(key, new CacheData(null));
 	}
 	
-	public boolean isContain(String key){
+	public boolean isContain(K key){
 		return this.cache.containsKey(key);
 	}
 	
@@ -84,17 +84,38 @@ public class RecordCache {
 		int count = 0;
 		long currDateTime = new Date().getTime();
 		logger.debug("init key enumeration");
-		Enumeration<String> keyEnum = cache.keys();
+		Enumeration<K> keyEnum = cache.keys();
 		logger.debug("init key enumeration done");
 		while(keyEnum.hasMoreElements()){
-			String key = keyEnum.nextElement();
-			if((currDateTime - cache.get(key).getTime()) >= recordExpiryTime * 60 * 1000){
+			K key = keyEnum.nextElement();
+			if((currDateTime - cache.get(key).getCacheTime().getTime()) >= recordExpiryTime * 60 * 1000){
 				logger.debug("remove key: {}", key);
 				cache.remove(key);
 				count++;
 			}
 		}
 		return count;
+	}
+	
+	private class CacheData{
+		V v;
+		Date cacheTime;
+		CacheData(V v){
+			this.v = v;
+			this.cacheTime = new Date();
+		}
+		public V getV() {
+			return v;
+		}
+		public void setV(V v) {
+			this.v = v;
+		}
+		public Date getCacheTime() {
+			return cacheTime;
+		}
+		public void setCacheTime(Date cacheTime) {
+			this.cacheTime = cacheTime;
+		}
 	}
 	
 	private class ClearTask implements Runnable{
@@ -111,5 +132,10 @@ public class RecordCache {
 			}
 		}
 		
+	}
+	
+	public static void main(String[] args) {
+		SelfClearRecordCache<String, Void> cache = new SelfClearRecordCache<String, Void>("cacheName");
+		cache.put("123");
 	}
 }
